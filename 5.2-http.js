beaab33cdeb4f7b9ad30e1710c9886eab5e2c030
wfk <525816762@163.com>
2024-01-11 22:44:17 +0800

    http

--- 5.2-http.js
+++ 5.2-http.js
@@ -1,5 +1,17 @@
 const http = require('http')
 const fs = require('fs')
+const path = require('path')
+const mimes = {
+  html: 'text/html',
+  css: 'text/css',
+  js: 'text/javascript',
+  png: 'image/png',
+  jpg: 'image/jpeg',
+  gif: 'image/gif',
+  mp4: 'video/mp4',
+  mp3: 'audio/mpeg',
+  json: 'application/json'
+}
 
 const server = http.createServer((request, response) => {
   //   response.setHeader('content-type', 'text/html;charset=utf-8')
@@ -29,16 +41,34 @@ const server = http.createServer((request, response) => {
 
   // 返回html
   // 解决html 引用 css js
-  if (pathname === '/') {
-    response.end(fs.readFileSync(__dirname + '/5.3-table.html'))
-  } else if (pathname === '/5.3-table.css') {
-    // 返回css 不能加response.setHeader('content-type', 'text/html;charset=utf-8') 否则会失效
-    response.end(fs.readFileSync(__dirname + '/5.3-table.css'))
-  } else if (pathname === '/5.3-table.js') {
-    response.end(fs.readFileSync(__dirname + '/5.3-table.js'))
-  } else {
-    response.end('<h1>404 Not Found</h1>')
-  }
+  // if (pathname === '/') {
+  //   response.end(fs.readFileSync(__dirname + '/5.3-table.html'))
+  // } else if (pathname === '/5.3-table.css') {
+  //   // 返回css 不能加response.setHeader('content-type', 'text/html;charset=utf-8') 否则会失效
+  //   response.end(fs.readFileSync(__dirname + '/5.3-table.css'))
+  // } else if (pathname === '/5.3-table.js') {
+  //   response.end(fs.readFileSync(__dirname + '/5.3-table.js'))
+  // } else {
+  //   response.end('<h1>404 Not Found</h1>')
+  // }
+
+  // 静态资源服务
+  const filePath = __dirname + pathname
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      response.statusCode = 500
+      response.end('<h1>404 Not Found</h1>')
+      return
+    }
+    // 设置 MIME 媒体类型
+    const ext = path.extname(filePath).slice(1)
+    console.log('ext', ext)
+    // 对于未知的资源类型，可以选择 application/octet-stream，浏览器会对响应体内容独立存储，也就是常见的下载效果
+    // 加 ;charset=utf-8 解决中文乱码问题
+    let type = mimes[ext] ? mimes[ext] + ';charset=utf-8' : 'application/octet-stream'
+    response.setHeader('content-type', type)
+    response.end(data)
+  })
 })
 
 server.listen(9000, () => {
